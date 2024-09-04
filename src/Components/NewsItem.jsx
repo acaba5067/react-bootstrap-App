f244bde6989da18743a582ea7a5fb3ada23a19c7
acaba5067 <acaba5067@gmail.com>
2024-09-04 21:45:14 +0300

    b

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import image from '../assests/logo.svg'
 function NewsItem({title,description,src,url}) {
     return (      
-        <div className="card bg-dark text-light ms-3 mt-3 mx-auto d-inline-block"style={{maxWidth:"345px"}}>
+        <div className="card bg-dark text-light  mb-3 d-inline-block mx-3 my-3 px-2 py-2"style={{maxWidth:"345px"}}>
         <img src={src?src:image} style={{height:"200px",width:"330px"}} className="card-img-top" alt="..."/>
           <div className="card-body">
         <h5 className="card-title">{title.slice(0,50)}</h5>
